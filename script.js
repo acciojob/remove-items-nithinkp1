44fe6d76cb580c8b1a6ec8f586af2a36003da7ed
nithinkp1 <131663803+nithinkp1@users.noreply.github.com>
2025-11-07 12:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.getElementById("button")
+const select = document.getElementById("colorSelect")
+let option = select.options;
+
+btn.addEventListener("click",clickMe)
+
+function clickMe(){
+	select.remove(option);
+	
+}
